590b203963cf03a13805ec6e32fab691162f6a09
Randika Wanninayaka <randika@arimaclanka.com>
2024-11-17 18:32:12 +0530

    added authenticated permission

--- product/views.py
+++ product/views.py
@@ -3,11 +3,13 @@ from .models import Product
 from .serializer import ProductSerializer
 from product import serializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def listproducts(request):
     query = Product.objects.all()
     serializer_class = ProductSerializer(query, many=True)
