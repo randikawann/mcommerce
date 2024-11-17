3509f52371dfb5a6a67a657918609cda3aa14996
Randika Wanninayaka <randika@arimaclanka.com>
2024-11-17 18:22:12 +0530

    added model serializer

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
+from .serializer import ProductSerializer
+from product import serializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def listproducts(request):
+    query = Product.objects.all()
+    serializer_class = ProductSerializer(query, many=True)
+    return Response(serializer_class.data)
