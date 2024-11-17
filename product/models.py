e9fed3c82578b3dcc5291c208e9cde8921f8dcbd
Randika Wanninayaka <randika@arimaclanka.com>
2024-11-17 17:46:21 +0530

    added product

--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.PositiveBigIntegerField()
+    name =  models.CharField(max_length=50)
+    cost = models.DecimalField(decimal_places=2, max_digits=6)
+    date = models.DateField()
+    description = models.TextField()
+
+
+    def __str__(self):
+        return self.name
+
