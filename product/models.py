e472e5bbf09752b20ac13ddcb69f026931a225c1
Randika Wanninayaka <randika@arimaclanka.com>
2024-11-18 09:07:21 +0530

    added new table product description and set foriegnkey

--- product/models.py
+++ product/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    category_name = models.CharField(max_length=30)
+    category_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.category_name
+
+
 class Product(models.Model):
+    category_name=models.ForeignKey('ProductCategory', related_name='ProductCategory', on_delete=models.CASCADE)
     product_id = models.PositiveBigIntegerField()
     name =  models.CharField(max_length=50)
     cost = models.DecimalField(decimal_places=2, max_digits=6)
