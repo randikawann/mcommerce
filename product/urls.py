e1f17d584b1a2caed8241f9eadb8db97e9a9dbe9
Randika Wanninayaka <randika@arimaclanka.com>
2024-11-18 09:26:28 +0530

    done ListProducts implementation

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from product import views
+from product.views import ListProducts
 
 urlpatterns = [
     path('productlist/', views.listproducts, name='ListProduct'),
+    path('classproductlist/', ListProducts.as_view(), name='Listproduct'),
 ]
--- product/views.py
+++ product/views.py
@@ -5,6 +5,7 @@ from product import serializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 # Create your views here.
 
@@ -14,3 +15,9 @@ def listproducts(request):
     query = Product.objects.all()
     serializer_class = ProductSerializer(query, many=True)
     return Response(serializer_class.data)
+
+class ListProducts(APIView):
+    def get(self, request):
+        query = Product.objects.all()
+        serializer_class = ProductSerializer(query, many=True)
+        return Response(serializer_class.data)
