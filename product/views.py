7a9777ec27d7f10121a3befa0e2959a30eb89d70
Randika Wanninayaka <randika@arimaclanka.com>
2024-11-18 09:58:05 +0530

    added post method

--- product/views.py
+++ product/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework import status
 
 # Create your views here.
 
@@ -28,3 +29,10 @@ class ProductDetailedView(APIView):
         query = Product.objects.filter(product_id = pid)
         serializer_class = ProductSerializer(query, many=True)
         return Response(serializer_class.data)
+
+    def post(self, request, pid):
+        serializer_obj = ProductSerializer(data = request.data)
+        if serializer_obj.is_valid(raise_exception = True):
+            product_saved = serializer_obj.save()
+            return Response({"success": "Product {} created successfully".format(product_saved.name)}, status=status.HTTP_200_OK)
+        return Response(serializer_obj.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
