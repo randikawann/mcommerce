cf91078ec9094c2d90191407ee842601bb14fe0f
Randika Wanninayaka <randika@arimaclanka.com>
2024-11-18 11:31:00 +0530

    moved post method and removed authentication

--- product/views.py
+++ product/views.py
@@ -11,7 +11,7 @@ from rest_framework import status
 # Create your views here.
 
 @api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def listproducts(request):
     query = Product.objects.all()
     serializer_class = ProductSerializer(query, many=True)
@@ -23,16 +23,16 @@ class ListProducts(APIView):
         serializer_class = ProductSerializer(query, many=True)
         return Response(serializer_class.data)
 
+    def post(self, request):
+        serializer_obj = ProductSerializer(data = request.data)
+        if serializer_obj.is_valid(raise_exception = True):
+            product_saved = serializer_obj.save()
+            return Response({"success": "Product {} created successfully".format(product_saved.name)}, status=status.HTTP_200_OK)
+        return Response(serializer_obj.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class ProductDetailedView(APIView):
     def get(self, request, pid):
         query = Product.objects.filter(product_id = pid)
         serializer_class = ProductSerializer(query, many=True)
         return Response(serializer_class.data)
-
-    def post(self, request, pid):
-        serializer_obj = ProductSerializer(data = request.data)
-        if serializer_obj.is_valid(raise_exception = True):
-            product_saved = serializer_obj.save()
-            return Response({"success": "Product {} created successfully".format(product_saved.name)}, status=status.HTTP_200_OK)
-        return Response(serializer_obj.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
