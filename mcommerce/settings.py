2efda19a93aca903c172ebd74fcfb9b04921bcf0
Randika Wanninayaka <randika@arimaclanka.com>
2024-11-27 17:00:10 +0530

    removed unwanted details

--- mcommerce/settings.py
+++ mcommerce/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-#'django-insecure-g%&c=4i+t%gpg*^tbb-q2@_(&l39+$9v!h+ni=41y)w(@1echb'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
